ac2c66ebdad925417b8d0ea8763a1f827cf9967b
Riley Watts and Stormi Hoebelheinrich <stormi@>
2017-04-26 13:52:37 -0700

    add delete functionality

--- app/routes/post.js
+++ app/routes/post.js
@@ -4,4 +4,13 @@ export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('post', params.post_id);
   },
+
+  actions: {
+    delete(post){
+      if(confirm("Are you sure you want to delete this post?")){
+        post.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
+  }
 });
